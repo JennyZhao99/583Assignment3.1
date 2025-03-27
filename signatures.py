6103e0b3380a733b06424afd51fefeae0b72e40a
JennyZhao99 <yibozh@seas.upenn.edu>
2025-03-26 20:41:29 -0700

    add sign and verify function codes

--- signatures.py
+++ signatures.py
@@ -7,13 +7,14 @@ def sign(m):
     w3 = Web3()
 
     # TODO create an account for signing the message
-    account_object = 0  # Create an Eth account
-    public_key = 0  # Eth account public key
-    private_key = 0  # Eth account private key
+    account_object = eth_account.Account.create()  # Create an Eth account
+    public_key = account_object.address  # Eth account public key
+    private_key = account_object.key  # Eth account private key
+
 
     # TODO sign the given message "m"
-    message = m  # Encode the message
-    signed_message = 0  # Sign the message
+    message = encode_defunct(text=m)  # Encode the message
+    signed_message = account_object.sign_message(message)  # Sign the message
 
 
     """You can save the account public/private keypair that prints in the next section
@@ -33,9 +34,9 @@ def verify(m, public_key, signed_message):
     w3 = Web3()
 
     # TODO verify the 'signed_message' is valid given the original message 'm' and the signers 'public_key'
-    message = m  # Encode the message
-    signer = 0  # Verify the message
-    valid_signature = 0  # True if message verifies, False if message does not verify
+    message = encode_defunct(text=m)  # Encode the message
+    signer = eth_account.Account.recover_message(message, signature=signed_message.signature)  # Verify the message
+    valid_signature = (signer.lower() == public_key.lower())  # True if message verifies, False if message does not verify
 
     assert isinstance(valid_signature, bool), "verify should return a boolean value"
     return valid_signature
